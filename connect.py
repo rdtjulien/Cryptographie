58204948b4f6a8a7daa41e14d4593283fa0f3846
JohnBlakeVS <jirokomahou@gmail.com>
2025-02-25 11:17:59 +0100

    MAJ

--- connect.py
+++ connect.py
@@ -11,6 +11,26 @@ except Exception as e:
     print("Cannot connect to the server:", e)
 print("Connected")
 
+
+#Partie Tranfo mot en byte
+
+# entrer le texte qu'on veut
+print("mot de base")
+message1 = 'hello worldé'
+print(message1)
+# transforme en byte char by char
+print("convertion byte")
+message1 = message1.encode('utf-8')
+print(message1)
+# mettre les byte dans un tableau
+print("mise en tableau")
+message1 = list(message1)
+print(message1)
+#transformer en Int
+#faire les manipulations sur les INT
+#re transformer en Byte
+
+#mettre en forme le message et transmettre
 message = 'task shift encode 6'
 length_string = int(len(message))
 bytes_val = length_string.to_bytes(2,'big')
